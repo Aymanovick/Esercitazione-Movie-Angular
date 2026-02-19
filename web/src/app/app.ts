ffff518f001749db226e402de0ecb4e177ab714b
Aymanovick <husseiniayman13@gmail.com>
2026-02-19 17:49:46 +0100

    feature: Aggiunta del service

--- web/src/app/app.ts
+++ web/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { MovieList } from './components/movie/movie-list/movie-list';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MovieList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
