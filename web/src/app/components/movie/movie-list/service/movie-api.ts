69674fbb4d55e3df65385c05061616a6d56683a4
Aymanovick <husseiniayman13@gmail.com>
2026-02-26 15:28:49 +0100

    feature: Configurato movie-api.ts

--- web/src/app/components/movie/movie-list/service/movie-api.ts
+++ web/src/app/components/movie/movie-list/service/movie-api.ts
@@ -1,8 +1,19 @@
-import { Injectable } from '@angular/core';
+import { HttpBackend, HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Movie } from '../../../../models/Movie';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MovieApi {
-  
+
+  readonly API_URL: string = 'http://localhost:8000/api/v1/movies'
+  private http: HttpClient = inject(HttpClient)
+
+  constructor() { }
+
+  getMovieList(): Observable<Movie[]> {
+    return this.http.get<Movie[]>(`${this.API_URL}/movies/`)
+  }
 }
