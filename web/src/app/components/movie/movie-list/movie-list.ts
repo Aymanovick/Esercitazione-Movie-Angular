351114e66e880a395d67b1bb31536b038290ac2a
Aymanovick <husseiniayman13@gmail.com>
2026-02-26 15:49:34 +0100

    feature: Presentazione dei dati del Backend

--- web/src/app/components/movie/movie-list/movie-list.ts
+++ web/src/app/components/movie/movie-list/movie-list.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Movie } from '../../../models/Movie';
+import { MovieApi } from './service/movie-api';
+import { Subscription } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-movie-list',
@@ -6,6 +10,31 @@ import { Component } from '@angular/core';
   templateUrl: './movie-list.html',
   styleUrl: './movie-list.css',
 })
-export class MovieList {
+export class MovieList implements OnInit, OnDestroy {
+  movieList!: Movie[];
+  private subscription: Subscription = new Subscription();
 
+  constructor(private api: MovieApi) {}
+
+  ngOnInit(): void {
+    this.onGetMovieList();
+  }
+
+  onGetMovieList(): void {
+    this.subscription.add(
+      this.api.getMovieList().subscribe({
+        next: (movieList: Movie[]): void => {
+          this.movieList = movieList;
+        },
+        error: (e: HttpErrorResponse) => {
+          throw Error(
+            `Cannot connect to API: Error: ${e.status} - ${e.message}`
+          );
+        }
+      })
+    );
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
